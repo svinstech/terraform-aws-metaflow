8e877c5e4f91d03085fc19abd2b838b62684e246
Lukasz Rohde <rohde@devopsbay.io>
2023-07-24 18:03:54 +0200

    feat: move security groups to ec2 launch template

--- modules/computation/ec2.tf
+++ modules/computation/ec2.tf
@@ -22,6 +22,10 @@ resource "aws_launch_template" "cpu" {
     associate_public_ip_address = var.with_public_ip
   }
 
+  vpc_security_group_ids = concat([
+    aws_security_group.this.id,
+  ], var.compute_environment_additional_security_group_ids)
+
   block_device_mappings {
     device_name = "/dev/xvda"
 
